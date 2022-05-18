08be0baea2fd66c4bb19fa2c220ed30a7a8abd10
Miriam <miriamsade11@gmail.com>
2022-05-18 12:48:37 +0300

    Hiding secret settings

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from .secret_settings import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u!*h*v3gr^215h1ep@$ptrrma8p%=p_86_mr&gjfk)$57d_uqm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
