7fd42db84eb40146073c80dc1e1110b8d0419c6b
Miriam <miriamsade11@gmail.com>
2022-05-20 12:58:12 +0300

    Added custom user profile midel, manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
 import os
-from .secret_settings import *
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# Read an environment variable.
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -119,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfiles'
